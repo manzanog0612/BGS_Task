b786cb1d0c0cc93084b1220acba0eea83ae70254
Guillermina Manzano <manzanog0612@gmail.com>
2024-03-27 19:54:26 -0300

    feat(gameplay): add gameplay scene with art

---
+++ Assets/Gameplay/Scenery/Scripts/TileData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace BGS_Task.Gameplay.Scenery
+{
+    [CreateAssetMenu(fileName = "Tile_", menuName = "ScriptableObjects/Scenery/Tile", order = 1)]
+    public class TileData
+    {
+        [SerializeField] private string id = null;
+        [SerializeField] private Sprite sprite = null;
+
+        public string Id { get => id; }
+        public Sprite Sprite { get => sprite; }
+    }
+}
+
---
+++ Assets/Gameplay/Scenery/Scripts/TileView.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace BGS_Task.Gameplay.Scenery
+{
+	public class TileView : MonoBehaviour
+	{
+		[SerializeField] private SpriteRenderer renderer = null;
+
+		private string id = null;
+
+        public string Id { get => id; }
+
+        public void Configure(TileData tileData)
+		{
+			id = tileData.Id;
+			renderer.sprite = tileData.Sprite;
+		}
+
+		public void ToggleView(bool status)
+		{
+			gameObject.SetActive(status);
+		}
+	}
+}
+
