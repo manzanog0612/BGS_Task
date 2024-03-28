3bf127ef7c37da34aa237a71a0484c2a72e430de
Guillermina Manzano <manzanog0612@gmail.com>
2024-03-27 21:01:53 -0300

    fix(gameplay): add characters sprites

--- Assets/Gameplay/PlayerController/Scripts/PlayerController.cs
+++ Assets/Gameplay/PlayerController/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ namespace BGS_Task.Gameplay.Player
     public class PlayerController : CharacterController
     {
         #region EXPOSED_FIELDS
+        [SerializeField] private float movementThreshold = 0.1f;
         #endregion
 
         #region PRIVATE_FIELDS
@@ -15,7 +16,18 @@ namespace BGS_Task.Gameplay.Player
         #region UNITY_CALLS
         private void LateUpdate()
         {
-            Move(new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")));
+            Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+            //player moves in only 1 direction at time
+            if (Mathf.Abs(movement.x) > movementThreshold)
+            {
+                movement.y = 0; 
+                Move(movement);
+            }
+            else if (Mathf.Abs(movement.y) > movementThreshold)
+            {
+                Move(movement);
+            }
         }
         #endregion
 
